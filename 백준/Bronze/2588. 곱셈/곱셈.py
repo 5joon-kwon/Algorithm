d4be9541696dcad7d72b261fa24d09871ca8f844
OhJoon Kwon <134563670+5joon-kwon@users.noreply.github.com>
2024-08-07 14:21:27 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,7 @@
+n = input()
+m = input()
+first = int(n) * int(m[2])
+second = int(n) * int(m[1])
+third = int(n) * int(m[0])
+result = first + second * 10 + third * 100
+print(first, second, third, result)
