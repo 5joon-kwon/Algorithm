61c78307fc36d9aaa36b578d12bd97a0e16a24d4
OhJoon Kwon <134563670+5joon-kwon@users.noreply.github.com>
2024-09-30 23:15:35 +0900

    [Silver IV] Title: 듣보잡, Time: 2392 ms, Memory: 43920 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,28 @@
+N, M = map(int, input().split())
+
+never = {}
+
+for _ in range(N):
+    never[input()] = 0
+    
+for _ in range(M):
+    name = input()
+    if name in never:
+        never[name] += 1
+    else:
+        never[name] = 0
+
+# lambda로 간단하게 표현
+# li = list(filter(lambda key: never[key] == 1, never.keys()))
+
+li = []
+
+for key, value in never.items():
+    if value == 1:
+        li.append(key)
+
+res = sorted(li)
+
+print(len(res))
+for i in res:
+    print(i)
