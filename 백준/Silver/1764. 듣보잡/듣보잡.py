feea2948fb6b54d47a961e56b7b6c773afdf19ba
OhJoon Kwon <134563670+5joon-kwon@users.noreply.github.com>
2024-10-01 16:42:50 +0900

    듣보잡.py

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -31,3 +31,30 @@ res = sorted(li)
 print(len(res))
 for i in res:
     print(i)
+
+
+# 집합으로 푼 풀이
+# import sys
+
+# N, M = map(int, sys.stdin.readline().strip().split())
+
+# set1 = set()
+# set2 = set()
+
+# # 듣도 못한 사람
+# for _ in range(N):
+#     set1.add(sys.stdin.readline().strip())
+
+# # 보도 못한 사람
+# for _ in range(M):
+#     set2.add(sys.stdin.readline().strip())
+
+# # 듣보잡들
+# result = sorted(list(set1 & set2))
+
+# # 듣보잡 수
+# print(len(result))
+
+# # 듣보잡 이름
+# for x in result:
+#     print(x)
