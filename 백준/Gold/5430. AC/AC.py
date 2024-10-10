34da769687b5c037047e934fc33a2d5d31d87590
OhJoon Kwon <134563670+5joon-kwon@users.noreply.github.com>
2024-10-10 20:43:23 +0900

    Reverse를 마지막 한번만 하여 시간을 줄인다 !

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -7,6 +7,7 @@ for _ in range(T):
     li = sys.stdin.readline().strip()
     num = li[1:-1]
     flag = 0
+    # Reverse에 대한 flag를 하나 만들었다.
     rev = 0
     
     if ',' in num:
@@ -22,8 +23,10 @@ for _ in range(T):
                 flag = 1
                 break
             else:
+                # R이 짝수번 나왔다면 list에서 처음 요소를 제거한다.
                 if rev % 2 == 0:
                     num.pop(0)
+                # # R이 홀수번 나왔다면 list에서 마지막 요소를 제거한다.
                 else:
                     num.pop()
     
@@ -34,6 +37,8 @@ for _ in range(T):
             res = ''.join(num)
             print('[' + res + ']')
         elif rev % 2 != 0:
+            # R이 홀수번 나왔다면 list의 요소를 뒤집어 준다.
+            # 최종적으로 한번만 list를 뒤집어 주면 된다 !
             num.reverse()
             res = ','.join(num)
             print('[' + res + ']')
