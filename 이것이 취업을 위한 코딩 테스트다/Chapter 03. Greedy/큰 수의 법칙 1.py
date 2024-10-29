5583a1e98681458faabc31712961d0082f238956
OhJoon Kwon <134563670+5joon-kwon@users.noreply.github.com>
2024-10-29 21:38:24 +0900

    예제 3-2. 단순 반복문으로 푸는 정답

--- 이것이 취업을 위한 코딩 테스트다/Chapter 03. Greedy/큰 수의 법칙 1.py
+++ 이것이 취업을 위한 코딩 테스트다/Chapter 03. Greedy/큰 수의 법칙 1.py
@@ -3,7 +3,7 @@ data = list(map(int, input().split()))
 
 data.sort()
 
-# 만약 가장 큰 수가 2개 이상이라면 index가 서로 다르므로 두 수를 번갈아 K 번씩 더하면 된다.
+# 만약 가장 큰 수가 2개 이상이라면, 큰 수끼리는 index가 서로 다르므로 두 수를 번갈아 K 번씩 더하면 된다.
 # 그게 아니라면 서로 다른 수를 K 번씩 더하면 된다.
 first = data[N - 1]
 second = data[N - 2]
