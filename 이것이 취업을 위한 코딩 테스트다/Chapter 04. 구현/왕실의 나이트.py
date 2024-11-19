8ef41e13fae6b4a7461fe5f8d482e6825e13d9e9
5joon-kwon <imjoon7@naver.com>
2024-11-19 20:33:28 +0900

    예제 4-3. 나의 풀이

--- 이것이 취업을 위한 코딩 테스트다/Chapter 04. 구현/왕실의 나이트.py
+++ 이것이 취업을 위한 코딩 테스트다/Chapter 04. 구현/왕실의 나이트.py
@@ -1,18 +1,32 @@
-input_data = input()
-row = int(input_data[1])
-column = int(ord(input_data[0])) - int(ord('a')) + 1
+cord = input()
+low = int(cord[1])
+col = cord[0]
 
-# 나이트가 이동할 수 있는 8가지 방향 정의
-steps = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
+low_list = [1, 2, 3, 4, 5, 6, 7, 8]
+col_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
-# 8가지 방향에 대하여 각 위치로 이동이 가능한지 확인
-result = 0
-for step in steps:
-    # 이동하고자 하는 위치 확인
-    next_row = row + step[0]
-    next_column = column + step[1]
-    # 해당 위치로 이동이 가능하다면 카운트 증가
-    if next_row >= 1 and next_row <= 8 and next_column >= 1 and next_column <= 8:
-        result += 1
+x = col_list.index(col)
+y = low_list.index(low)
+res = 0
 
-print(result)
+# 1. 수평으로 두칸 이동한 뒤에 수직으로 한 칸 이동하기
+if x - 2 >= 0 and y - 1 >= 0:
+    res += 1
+if x - 2 >= 0 and y + 1 <= 7:
+    res += 1
+if x + 2 <= 7 and y - 1 >= 0:
+    res += 1
+if x + 2 <= 7 and y + 1 <= 7:
+    res += 1
+
+# 2. 수직으로 두칸 이동한 뒤에 수평으로 한 칸 이동하기
+if y - 2 >= 0 and x - 1 >= 0:
+    res += 1
+if y - 2 >= 0 and x + 1 <= 7:
+    res += 1
+if y + 2 <= 7 and x - 1 >= 0:
+    res += 1
+if y + 2 <= 7 and x + 1 <= 7:
+    res += 1
+
+print(res)
