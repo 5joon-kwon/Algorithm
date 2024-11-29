f259a4d164763275d7bb9d2134b0116986fa4905
OhJoon Kwon <134563670+5joon-kwon@users.noreply.github.com>
2024-11-29 22:22:57 +0900

    예제 5-9. BFS

--- 이것이 취업을 위한 코딩 테스트다/Chapter 05. DFS.BFS/5-9. BFS 예제.py
+++ 이것이 취업을 위한 코딩 테스트다/Chapter 05. DFS.BFS/5-9. BFS 예제.py
@@ -10,6 +10,7 @@ def bfs(graph, start, visited):
             if not visited[i]:
                 queue.append(i)
                 visited[i] = True
+                
 graph = [
     [],
     [2, 3, 8],
