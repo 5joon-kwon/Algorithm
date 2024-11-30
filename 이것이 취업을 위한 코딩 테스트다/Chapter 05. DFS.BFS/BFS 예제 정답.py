126c219057898c40d69fe7aab35f77f70177f413
OhJoon Kwon <134563670+5joon-kwon@users.noreply.github.com>
2024-11-30 22:54:13 +0900

    예제 5-9. BFS 정답

--- 이것이 취업을 위한 코딩 테스트다/Chapter 05. DFS.BFS/BFS 예제 정답.py
+++ 이것이 취업을 위한 코딩 테스트다/Chapter 05. DFS.BFS/BFS 예제 정답.py
@@ -6,7 +6,7 @@ def bfs(graph, start, visited):
     queue = deque([start])
     # 현재 노드를 방문 처리
     visited[start] = True
-    # 큐가 빌 때까지 반복
+    # 큐(Queue)가 빌 때까지 반복
     while queue:
         # 큐에서 하나의 원소를 뽑아 출력
         v = queue.popleft()
