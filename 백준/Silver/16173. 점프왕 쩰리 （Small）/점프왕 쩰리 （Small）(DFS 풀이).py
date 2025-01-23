3433b370ee180a05656be7770c5b6cdcf02809d3
OhJoon Kwon <134563670+5joon-kwon@users.noreply.github.com>
2025-01-23 22:36:46 +0900

    DFS 활용 풀이

--- 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）(DFS 풀이).py
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）(DFS 풀이).py
@@ -7,16 +7,19 @@ visit = [[0] * N for _ in range(N)]
 def dfs(x, y):
     if x < 0 or x >= N or y < 0 or y >= N:
         return
-    
+
+    # 이미 방문했거나, 0칸 이동이면
     if visit[x][y] == 1 or graph[x][y] == 0:
         return
-    
+
+    # 목적지에 도착했다면
     if graph[x][y] == -1:
         visit[x][y] = 1
         return
     
     visit[x][y] = 1
-    
+
+    # 현재 위치에서 이동한 위치로 재귀
     dfs(x + graph[x][y], y)
     dfs(x, y + graph[x][y])
 
