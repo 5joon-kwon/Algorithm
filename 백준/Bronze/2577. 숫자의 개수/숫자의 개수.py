4f7b56ad9c536b8b495b468f7bfa2a9b31fd96a5
OhJoon Kwon <134563670+5joon-kwon@users.noreply.github.com>
2025-01-29 22:53:41 +0900

    [Bronze II] Title: 숫자의 개수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -0,0 +1,17 @@
+A = int(input())
+B = int(input())
+C = int(input())
+
+mul = A * B * C
+li = list(str(mul))
+
+print(li.count('0'))
+print(li.count('1'))
+print(li.count('2'))
+print(li.count('3'))
+print(li.count('4'))
+print(li.count('5'))
+print(li.count('6'))
+print(li.count('7'))
+print(li.count('8'))
+print(li.count('9'))
