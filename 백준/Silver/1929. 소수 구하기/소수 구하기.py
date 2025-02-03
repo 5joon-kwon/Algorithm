e4f693d6141841142aa5c7d306b5940e6bc93299
OhJoon Kwon <134563670+5joon-kwon@users.noreply.github.com>
2025-02-03 18:53:43 +0900

    [Silver III] Title: 소수 구하기, Time: 208 ms, Memory: 40084 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,12 +1,12 @@
 M, N = map(int, input().split())
 
 def is_prime(N):
-    # 1 ~ N 까지 모두 판별하기 위해 N + 1개 index 설정정
+    # 1 ~ N 까지 모두 판별하기 위해 N + 1개 index 설정
     prime = [True] * (N + 1)
     # index 0, 1은 소수가 아니므로 False 처리
     prime[0] = prime[1] = False
     
-    # 2부터 N의 제곱근까지 소수 판별별
+    # 2부터 N의 제곱근까지 소수 판별
     for i in range(2, int(N ** 0.5) + 1):
         # i 번째 숫자가 True (소수) 이면
         if prime[i]:
@@ -20,6 +20,6 @@ def is_prime(N):
 prime_list = is_prime(N)
 
 for i in range(M, N + 1):
-    # True(소수)이면 출력력
+    # True(소수)이면 출력
     if prime_list[i]:
         print(i)
