b2d8d59e72a45b7ea63177ca4ae79e058f4e3f4f
OhJoon Kwon <134563670+5joon-kwon@users.noreply.github.com>
2025-08-04 20:52:33 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,13 +1,18 @@
 N, K = map(int, input().split())
 
 num_list = [i for i in range(1, N + 1)]
+
 start = 0
+
 res = []
 
 while len(num_list) != 0:
     order = start + K - 1
-    while order >= len(num_list):
+    while True:
+        if order >= len(num_list):
             order -= len(num_list)
+        if order < len(num_list):
+            break
     res.append(num_list.pop(order))
     start = order
 
