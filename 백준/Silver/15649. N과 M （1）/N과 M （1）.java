64aa5cf569c497ddfc0ddaa61b6b96e1f8e8beda
OhJoon Kwon <134563670+5joon-kwon@users.noreply.github.com>
2025-08-27 11:46:23 +0900

    [Silver III] Title: N과 M (1), Time: 1872 ms, Memory: 62324 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -2,51 +2,42 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static List<Integer> answer = new ArrayList<>();
-	static boolean[] visited;
-	static int N, M;
+    static List<Integer> answer = new ArrayList<>();
+    static boolean[] visited;
+    static int N, M;
 
-	public static void print() {
-		for (int i = 0; i < answer.size(); i++) {
-			System.out.print(answer.get(i) + " ");
-		}
-		System.out.println();
-	} //print
+    static void print() {
+        for (int x : answer) 
+	        System.out.print(x + " ");
+        System.out.println();
+    }
 
-	public static void choose(int c) {
-		if (c > N + 1) {
-			if (answer.size() == M) {
-				print();
-				return;
-			}
-			return;
-		}
+    static void choose() {                 // ← c 제거
+        if (answer.size() == M) {          // 길이 M이면 출력
+            print();
+            return;
+        }
+        
+        for (int i = 1; i <= N; i++) {     // 매 단계에서 1..N 전체 탐색
+            if (!visited[i]) {
+                visited[i] = true;
+                answer.add(i);
 
-		if (answer.size() == M) {
-			print();
-			return;
-		}
+                choose();                  // 다음 자리 채우기
 
-		for (int i = 1; i <= N; i++) {
-			if (!visited[i]) {
-				answer.add(i);
-				visited[i] = true;
+                answer.remove(answer.size() - 1);
+                visited[i] = false;        // 복구
+            }
+        }
+    }
 
-				choose(i + 1);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N + 1];
 
-				answer.remove(answer.size() - 1);
-				visited[i] = false;
-			}
-		}
-	} //choose
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		visited = new boolean[N + 1];
-
-		choose(1);
-	} //main
-} //class
+        choose();
+    }
+}
