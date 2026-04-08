0754b2aa26af5c56f79da77c4bd817936a11c591
OhJoon Kwon <134563670+5joon-kwon@users.noreply.github.com>
2026-04-08 20:04:08 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 152 ms, Memory: 37724 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -3,7 +3,7 @@ from collections import deque
 n, k = map(int, input().split())
 
 Max = 100001
-dist = [-1] * Max
+dist = [float('INF')] * Max
 q = deque()
 
 dist[n] = 0
@@ -12,21 +12,19 @@ q.append(n)
 while q:
     next = q.popleft()
 
-    if next == k:
-        print(dist[next])
-        break
-
     nx = next * 2
-    if 0 <= nx < Max and dist[nx] == -1:
+    if 0 <= nx < Max and dist[nx] > dist[next]:
         q.appendleft(nx)
         dist[nx] = dist[next]
 
     nx = next - 1
-    if 0 <= nx < Max and dist[nx] == -1:
+    if 0 <= nx < Max and dist[nx] > dist[next] + 1:
         q.append(nx)
         dist[nx] = dist[next] + 1
         
     nx = next + 1
-    if 0 <= nx < Max and dist[nx] == -1:
+    if 0 <= nx < Max and dist[nx] > dist[next] + 1:
         q.append(nx)
         dist[nx] = dist[next] + 1
+
+print(dist[k])
