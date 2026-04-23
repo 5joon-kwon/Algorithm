ef2b08342444831a05a30217b5a8052a9c34047c
OhJoon Kwon <134563670+5joon-kwon@users.noreply.github.com>
2026-04-23 20:40:49 +0900

    [Bronze I] Title: 색종이, Time: 328 ms, Memory: 39212 KB, Score: 53 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,12 +1,19 @@
-<html>
-<head><title>502 Bad Gateway</title></head>
-<body>
-<center><h1>502 Bad Gateway</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+n = int(input())
+grid = [[0] * 1002 for _ in range(1002)]
+
+idx = 1
+for _ in range(n):
+    lx, ly, w, h = map(int, input().split())
+    for y in range(ly, ly + h):
+        for x in range(lx, lx + w):
+            grid[y][x] = idx
+    idx += 1
+
+ans = [0] * (n + 1)
+for i in range(1002):
+    for j in range(1002):
+        if grid[i][j] != 0:
+            ans[grid[i][j]] += 1
+
+for i in ans[1:]:
+    print(i)
